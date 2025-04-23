230a14e661d91577a3a1112d94480a2120053eaa
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-04-23 17:28:20 +0200

    interface blog

---
+++ server/src/blog.ts
@@ -0,0 +1,12 @@
+import * as mongodb from "mongodb";
+
+export interface Blog {
+    titolo: string;
+    descrizione: string;
+    categoria: "Informatica" | "Crescita Personale" | "Lavoro" | "Consigli" | "Riflessioni";
+    autore: string;
+    immagine: string;
+    data: Date;
+    minuti_lettura: string
+    _id?: mongodb.ObjectId;
+}
