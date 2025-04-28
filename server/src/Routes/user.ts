1f26796755358e67ddb37c06a9bb0115613c2b07
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-04-28 17:44:53 +0200

    inizio dell'authentication

---
+++ server/src/Routes/user.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const router = express.Router();
+
+//rotta per la registrazione --- SIGNUP ROUTE
+
+router.post("/signup", (req,res,next) => {
+   
+})
