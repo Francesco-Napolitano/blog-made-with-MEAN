805a651dc9d2efb18ecce62b77852fcd860d0861
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-05-06 15:54:43 +0200

    test signal post service

--- client/src/app/features/post/allpost/allpost.component.ts
+++ client/src/app/features/post/allpost/allpost.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal, Signal } from '@angular/core';
+import { PostService } from '../../../services/post/post.service';
 
 @Component({
   selector: 'app-allpost',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './allpost.component.html',
   styleUrl: './allpost.component.scss',
 })
-export class AllpostComponent {}
+export class AllpostComponent implements OnInit {
+  posts = signal({});
+  private postService = inject(PostService);
+  //è la stessa cosa che scrivere i constructor private ma in questo modo riduci le righe ed è più efficiente
+
+  ngOnInit(): void {
+    this.postService
+      .getAllPosts()
+      .subscribe((allPost) => this.posts.set(allPost));
+  }
+}
