7cddb666491df48ed28025bfcc2fa17f1f4d27b1
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-05-23 17:36:14 +0200

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
